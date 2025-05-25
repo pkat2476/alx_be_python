2cf439458605681f58e86f0a162d5712b6529ad6
Paul KATO <paul.katom@gmail.com>
2025-05-25 17:57:01 +0300

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -9,3 +9,13 @@ difference: 5
 >>> product = number1 * number2
 >>> print (product:, product)
 product: 50
+
+
+number1 = 10
+number2 = 5
+total = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+print("Addition of",number1, "and", number2, "is", total)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
