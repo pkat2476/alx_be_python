d41886af408803fd6dc0fd8cdb26190fb640431c
Paul KATO <paul.katom@gmail.com>
2025-05-25 18:46:43 +0300

    basic operation

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,11 @@
-
+>>> number1 = 10
+>>> number2 = 5
+>>> total = number1 + number2
+>>> difference = number1 - number2
+>>> product = number1 * number2
+>>> print(Addition of,number1, and, number2, is, total)
+Addition of 10 and 5 is 15
+>>> print(Subtraction of, number1, and, number2, is, difference)
+Subtraction of 10 and 5 is 5
+>>> print(Multiplication of, number1, and, number2, is, product)
+Multiplication of 10 and 5 is 50
