a45e11ac3b46f8b4b1be2b35d049f148f9fbdb57
Paul KATO <paul.katom@gmail.com>
2025-05-25 16:17:31 +0300

    Add files via upload

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+>>> # Assigned specific values to number1 and number2
+>>> number1 = 10
+>>> number2 = 5
+>>>
+>>> # Perform  basic arithmetic operations
+>>> sum = number1 + number2
+>>> print (sum:, sum)
+sum: 15
+>>>
+>>> difference = number1 - number2
+>>> print (difference:, difference)
+difference: 5
+>>> product = number1 * number2
+>>> print (product:, product)
+product: 50
+>>>
---
+++ basic_operationss.py
@@ -0,0 +1,1 @@
+python_introduction/basic_operations.py
